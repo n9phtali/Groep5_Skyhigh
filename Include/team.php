1bbc0aea82ebabc825eb6319ad273b3e263b11eb
Naph <naphtalipietersz@gmail.com>
2023-06-13 11:15:32 +0200

    moved foto's to img

--- Include/team.php
+++ Include/team.php
@@ -1,11 +1,11 @@
 <section id="team">
     <h1 class="headings">TEAM</h1>
     <div class="gallery">
-        <img src="../img/Foto's/SCHLEICHER_ASK_21_vr.jpg" alt="" />
-        <img src="../img/Foto's/1small.jpg" alt="" />
-        <img src="../img/Foto's/Grob_103_79N.jpg" alt="" />
-        <img src="../img/Foto's/MDM FOX.jpeg" alt="" />
-        <img src="../img/Foto's/OY-FBX_Scheibe_Bergfalke_III.jpg" alt="" />
-        <img src="../img/Foto's/Gliders.jpg" alt="" />
+        <img src="../img/SCHLEICHER_ASK_21_vr.jpg" alt="" />
+        <img src="../img/1small.jpg" alt="" />
+        <img src="../img/Grob_103_79N.jpg" alt="" />
+        <img src="../img/MDM%20FOX.jpeg" alt="" />
+        <img src="../img/OY-FBX_Scheibe_Bergfalke_III.jpg" alt="" />
+        <img src="../img/Gliders.jpg" alt="" />
     </div>
 </section>
