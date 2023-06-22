7957530c191802f1eaac5906173eb2e5590f49c0
Naph <naphtalipietersz@gmail.com>
2023-06-22 10:50:11 +0200

    logout aan navbar toegevoegd wanneer je ingelogd bent

--- include/navbar.php
+++ include/navbar.php
@@ -5,17 +5,23 @@
         <li><a href="team.php">Team</a></li>
         <li><a href="informatie.php">Informatie</a></li>
         <li><a href="contact.php">Contact</a></li>
-        <li><a href="Login_Signup.php">Account</a></li>
+
         <?php
         session_start();
         if (isset($_SESSION['login'])) {
             ?>
+            <li><a href="Login_Signup.php">Account</a></li>
             <li><a href="logout.php">logout</a></li>
             <?php
 
 
 
+        }else{
+            ?>
+        <li><a href="login_signup.php">Login</a></li>
+        <?php
         }
+
         ?>
     </ul>
 </nav>
