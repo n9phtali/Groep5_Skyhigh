045e5a018da3454c41b3fde3c23e300bcac5890f
Nizar Elmorabit <2112104@talnet.nl>
2023-06-22 11:21:36 +0200

    Added page account gegevens

---
+++ account_gegevens.php
@@ -0,0 +1,24 @@
+!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Contact</title>
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div class="account">
+    <div class="Gegevens">
+         <a href="#">Account gegevens</a>
+    </div>
+    <div class="vluchten">
+        <a href="#"> Mijn vluchten</a>
+    </div>
+    <div class="verwijderen">
+        <a href="#">Accountverwijderen</a>
+    </div>
+
+
+
+</div>
+</body>
--- database/conn.php
+++ database/conn.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$dbname = "skyhighh";
+$dbname = "database dragonfly";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
