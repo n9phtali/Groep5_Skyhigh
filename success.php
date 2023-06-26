260da2416fe093ee8d960a22791cac7130a9a369
Naph <naphtalipietersz@gmail.com>
2023-06-26 11:16:20 +0200

    contact pagina vernieuwd

---
+++ success.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Zweefvliegtuig club</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <link
+        rel="stylesheet"
+        media="screen and (max-width:800px)"
+        href="css/tablet.css"
+
+    />
+</head>
+<body>
+<?php include'include/navbar.php';?>
+<section id="informatie">
+    <h1 class="headings">SUCCESS</h1>
+    <div class="row">
+        <div class="box">
+            <img src="./img/Gliding_pictogram.svg.png" alt="" />
+            <h1 class="headings">
+                Still need <br />
+                something here
+            </h1>
+            <p>Still need something here</p>
+        </div>
+        <div class="box">
+            <img src="./img/Gliding_pictogram.svg.png" alt="" />
+            <h1 class="headings">
+                Still need <br />
+                something here
+            </h1>
+            <p>Still need something here</p>
+        </div>
+        <div class="box">
+            <img src="./img/Gliding_pictogram.svg.png" alt="" />
+            <h1 class="headings">
+                Still need <br />
+                something here
+            </h1>
+            <p>Still need something here</p>
+        </div>
+    </div>
+</section>
+</body>
+</html>
+
